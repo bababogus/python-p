9f585cabff777efac748161d8308a9db34749daf
amit <amit@ISTWLAPASUS1616.innovasolutions.local>
2018-03-15 12:11:27 +0800

    first commit

---
+++ example1.py
@@ -0,0 +1,17 @@
+print 'Here i am '
+print 'Doing some practice'
+
+# name= input ('whats your name : ' )
+# print name
+# age= input('whats your Age : ')
+# print age
+# year=str(2017-age+100)
+# print ('baba '+ name +', you will turn 100 years old on '+ year)
+
+a=['amit','mehta','is','learning','python']
+print a
+
+print a[0]+' '+a[1]+' '+a[2]+' '+a[3]+' '+a[4]
+
+a={'am':1}
+print a
